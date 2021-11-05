913eb553c0aa676ee46fa95888e2a1af16b69576
musayuksel <musayuxel@gmail.com>
2021-11-05 19:55:58 +0000

    feat: Get Album by ID

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { request, response } = require("express");
 const express = require("express");
 const albumsData = require("./data/albums.json");
 const app = express();
@@ -7,6 +8,21 @@ app.get("/albums", (request, response) => {
   response.send(albumsData);
 });
 
+// 3.2) Get Album by ID
+app.get("/albums/:albumID", (request, response) => {
+  const albumID = request.params.albumID;
+  const filteredAlbum = albumsData.filter(
+    (album) => album.albumId === albumID
+  );
+  if (filteredAlbum.length === 0) {
+    return response
+      .status(404)
+      .send(
+        `We're sorry, we couldn't find the album you requested. Please check your Id :${albumID}`
+      );
+  }
+  response.send(filteredAlbum);
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
