a2a5a398ff3dd2bec9b9c289921bf0016fdc714e
musayuksel <musayuxel@gmail.com>
2021-11-05 19:41:02 +0000

    feat: Add Get all albums

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const albumsData = require("./data/albums.json");
 const app = express();
 const PORT = process.env.PORT || 3000;
+//3.1) Get All Albums
+app.get("/albums", (request, response) => {
+  response.send(albumsData);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
